374b3469be23defaf8fc5184b024be2c606c1e01
Denis Efimov <denis.efimov@btsdigital.kz>
2018-11-21 13:08:05 +0600

    Initial Commit

---
+++ TestGradient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGradient
+//
+//  Created by Denis Efimov on 11/21/18.
+//  Copyright © 2018 Denis Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
